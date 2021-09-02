838ab826ea9e598cac8f4098183303db35c23c11
James Gallagher <jgallagher@opendap.org>
2021-09-01 22:20:14 -0600

    More changes to process_scope_data.py
    It's close to correct, but maybe the scope data is missing the real
    time the MCU wakes up? Also, remove old code the processes raw values.

--- process_scope_data.py
+++ process_scope_data.py
@@ -14,14 +14,17 @@ from scipy.signal import butter, filtfilt, sosfilt
 import plotly.graph_objects as go
 
 
+# For the 23dBm file, -d 000005 and -o 0.0; for the 13dBm file, -d 0.00001 -o 0.0765
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interval", help="read every Xth value", default=1000, type=int)
-    parser.add_argument("-z", "--zero", help="Any value < this is zero", default=0.03, type=float)
+    parser.add_argument("-i", "--interval", help="read every Xth value, ignored for 'filter'", default=1000, type=int)
+    parser.add_argument("-z", "--zero", help="Any value < this is zero", default=0.0, type=float)
     parser.add_argument("-s", "--skip", help="skip the first N lines", default=12, type=int)
     parser.add_argument("-r", "--resistance", help="Resistance used in measurement", default=2.2, type=float)
     parser.add_argument("-d", "--delta_t", help="Delta time for samples", default=0.000005, type=float)
+    parser.add_argument("-o", "--offset", help="Vertical offset", default=0.0, type=float)
     parser.add_argument("-w", "--what", help="Do what: calc mAs; print data, filter data", default='calc')
+    parser.add_argument("-p", "--plot", help="Plot the raw and filtered data", default=False, type=bool)
     parser.add_argument('data_file', help='Read from this file')
     args = parser.parse_args()
 
@@ -33,34 +36,37 @@ def main():
         # Filter requirements.
         # T = 14.0  # Sample Period, seconds
         fs = 100000.0  # sample rate, Hz
-        cutoff = 3000  # desired cutoff frequency of the filter, Hz
+        cutoff = 1000.0  # desired cutoff frequency of the filter, Hz
         # nyq = 0.5 * fs  # Nyquist Frequency
-        order = 5
+        order = 4
         # n = int(T * fs)  # total number of samples
 
         # data_file could be '/Users/jimg/src/opendap/HAST_leaf_node_data/Current_measurement/LN_Current_13dBm_23dBm/13dBm_current.csv'
 
         data = np.genfromtxt(args.data_file, delimiter=',', skip_header=args.skip)
+
         # slice so we have only the voltage values. 'data_file' holds both the sample time
-        # and the voltage as CSV data. Convert Volts to mA (voltage measured with a 2.2ohm resistor)
-        data = data[...,1] * 1000 / args.resistance
+        # and the voltage as CSV data.
+        data = data[...,1] + args.offset    # * 1000 / args.resistance
 
         y = butter_lowpass_filter(data, cutoff, fs, order)
 
-        calc_values_from_siglent_csv(args.data_file, args.zero, args.skip, args.resistance, args.delta_t)
-        
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(
-            y=data[::100],
-            line=dict(shape='spline'),
-            name='signal with noise'
-        ))
-        fig.add_trace(go.Scatter(
-            y=y[::100],
-            line=dict(shape='spline'),
-            name='filtered signal'
-        ))
-        fig.show()
+        if (args.plot):
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(
+                y=data[::100],
+                line=dict(shape='spline'),
+                name='signal with noise'
+            ))
+            fig.add_trace(go.Scatter(
+                y=y[::100],
+                line=dict(shape='spline'),
+                name='filtered signal'
+            ))
+            fig.show()
+
+        # data[...,0] slices just the time info
+        calc_values_from_filtered_data(y, args.zero, args.delta_t, args.resistance)
     else:
         args.usage()
 
@@ -91,6 +97,40 @@ def print_values_from_siglent_csv(data_file, zero_value, skip, sample_interval):
                 print(f"{time},{volts}", end='\n')
             line = in_file.readline()
             i += 1
+
+
+def calc_values_from_filtered_data(data, zero_value, delta_t, resistance):
+    """
+    @param data is corrected mV for a time slice
+    @param zero_value is the value to call 'zero mA'
+    @param delta_t is the duration of each sample
+    @param resistance Used to convert mV to mA
+    """
+    volts_samples = 0.0
+    samples = 0
+    start_time = 0.0
+    end_time = 0.0
+    sample = 0
+
+    for volts in data:
+        if volts > zero_value:
+            if start_time == 0.0:
+                start_time = sample * delta_t
+            end_time = sample
+            samples += 1         # count the samples, divide once outside of loop
+            volts_samples += volts     # As above, factor out '* delta_t'
+        sample += 1
+
+    # Complete calculations and print the results
+    end_time *= delta_t
+    total_time = samples * delta_t
+    # volts_samples / samples: average voltage/sample; * delta_t is avg voltage/second
+    # delta_t is seconds/sample --> avg voltage / sample * delta_t is voltage/second
+    # voltage/second / resistance is  Amps/ econd; * 1000 --> mA/s
+    # mA/s * total_time is mAs
+    mAs = (volts_samples / samples) / resistance * 1000
+
+    print(f"Start time: {start_time}, End time: {end_time}, Total time: {total_time}, mAs: {mAs}\n")
 
 
 def calc_values_from_siglent_csv(data_file, zero_value, skip, resistance, delta_t):
